0d784207a201b58bfb60cc32497048800dfb66ce
tchou0 <129346770+tchou0@users.noreply.github.com>
2023-06-26 21:26:41 -0400

    Update ShoppingCartController.java

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -25,11 +25,11 @@ public class ShoppingCartController {
     private ProductDao productDao;
 
     //============================== Constructor added =======================================
-    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
-        this.shoppingCartDao = shoppingCartDao;
-        this.userDao = userDao;
-        this.productDao = productDao;
-    }
+    // public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+    //     this.shoppingCartDao = shoppingCartDao;
+    //     this.userDao = userDao;
+    //     this.productDao = productDao;
+    // }
 
     // each method in this controller requires a Principal object as a parameter
     @GetMapping
